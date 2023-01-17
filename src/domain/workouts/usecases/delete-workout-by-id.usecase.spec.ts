6a4184cfea5fee2508807469f50dbdefe25d1900
Thiago Andrade Silva <thiago.adsix@gmail.com>
2023-01-17 00:47:37 -0300

    perf(test): improve the tests for DeleteWorkoutByIdUsecase

--- src/domain/workouts/usecases/delete-workout-by-id.usecase.spec.ts
+++ src/domain/workouts/usecases/delete-workout-by-id.usecase.spec.ts
@@ -1,27 +1,27 @@
+import { WorkoutRepository } from 'src/domain/protocols/repositories/workout.repository'
+import { dateMock } from '../mocks/date.mock'
+import { WorkoutRepositoryMock } from '../mocks/workout.repository.mock'
 import { DeleteWorkoutByIdUsecase } from './delete-workout-by-id.usecase'
 
 describe('DeleteWorkoutByIdUsecase', () => {
-  it('should delete a workout by id', async () => {
-    // mock the WorkoutRepository implementation
-    const mockWorkoutRepository = {
-      delete: jest.fn().mockResolvedValue(undefined),
-      save: jest.fn().mockResolvedValue(undefined),
-      findAll: jest.fn().mockResolvedValue(undefined),
-      findById: jest.fn().mockResolvedValue(undefined),
-      addExercise: jest.fn().mockResolvedValue(undefined)
-    }
+  let workoutRepository: WorkoutRepository
 
-    // create an instance of the usecase
-    const usecase = new DeleteWorkoutByIdUsecase(mockWorkoutRepository)
+  let sut: DeleteWorkoutByIdUsecase
 
-    // define the input variables
+  beforeEach(() => {
+    dateMock()
+
+    workoutRepository = new WorkoutRepositoryMock()
+    sut = new DeleteWorkoutByIdUsecase(workoutRepository)
+  })
+  it('should call WorkoutRepository.delete with correct values', async () => {
+    const workoutRepositoryDeleteSpy = jest.spyOn(workoutRepository, 'delete')
     const id = '123'
     const userId = '456'
 
-    // execute the usecase
-    await usecase.execute(id, userId)
+    await sut.execute(id, userId)
 
-    // assert that the delete method of the repository was called with the correct arguments
-    expect(mockWorkoutRepository.delete).toHaveBeenCalledWith(id, userId)
+    expect(workoutRepositoryDeleteSpy).toHaveBeenCalledWith(id, userId)
+    expect(workoutRepositoryDeleteSpy).toHaveBeenCalledTimes(1)
   })
 })
